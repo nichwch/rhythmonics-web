be4adec24fc423b5ba5c9371131b97e2c5e5601f
nichwch <nichwch@gmail.com>
2023-08-16 17:32:13 -0400

    UI stuff

---
+++ src/App.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  let speed = 0;
+  let one_enabled = false;
+  let two_enabled = false;
+  let three_enabled = false;
+  let four_enabled = false;
+  let five_enabled = false;
+  let six_enabled = false;
+  let seven_enabled = false;
+  let eight_enabled = false;
+
+  $: console.log({
+    speed,
+    one_enabled,
+    two_enabled,
+    three_enabled,
+    four_enabled,
+    five_enabled,
+    six_enabled,
+    seven_enabled,
+    eight_enabled,
+  });
+</script>
+
+<div>
+  <div class="border border-black m-10 p-2">
+    My ugly first draft of Rhythmonics!
+    <!-- speed input -->
+    <input bind:value={speed} />
+    <!-- 1 -->
+    <input type="checkbox" bind:checked={one_enabled} />
+    <!-- 2 -->
+    <input type="checkbox" bind:checked={two_enabled} />
+    <!-- 3 -->
+    <input type="checkbox" bind:checked={three_enabled} />
+    <!-- 4 -->
+    <input type="checkbox" bind:checked={four_enabled} />
+    <!-- 5 -->
+    <input type="checkbox" bind:checked={five_enabled} />
+    <!-- 6 -->
+    <input type="checkbox" bind:checked={six_enabled} />
+    <!-- 7 -->
+    <input type="checkbox" bind:checked={seven_enabled} />
+    <!-- 8 -->
+    <input type="checkbox" bind:checked={eight_enabled} />
+    <!-- overtones -->
+  </div>
+  <div>
+    current speed is {speed}
+    <button
+      on:click={(evt) => {
+        speed += 2;
+      }}
+    >
+      increment speed</button
+    >
+  </div>
+</div>
